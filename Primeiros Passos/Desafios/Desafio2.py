9328dd06d9da12b9999c3cb154645fe7465331dc
Guilherme Mattos <guilherme.mattos.gam@gmail.com>
2020-12-28 14:12:07 -0300

    atualizando

--- Primeiros Passos/Desafios/Desafio2.py
+++ Primeiros Passos/Desafios/Desafio2.py
@@ -1,9 +1,3 @@
-# O seu professor gostaria de fazer um programa com as seguintes características:
-
-# Leia uma data no formato DD/MM/AA;
-# Imprima a data no formato MM/DD/AA;
-# Imprima a data no formato AA/MM/DD;
-# Imprima a data no formato DD-MM-AA.
 
 data=input();
 A = (data[0:2]); # dia
